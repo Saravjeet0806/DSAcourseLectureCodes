6b015950137f4647ae0e52dbc3de7fca2ba8f161
Sarav <Saravjeetofficial@gmail.com>
2024-03-15 22:26:20 +0530

    cycleDetection bfs

--- lecture6/decimalTObinary.cpp
+++ lecture6/decimalTObinary.cpp
@@ -1,2 +1,25 @@
-//divide number by 2
-//store remainder and repeat process until n!=0;
+// divide number by 2
+// store remainder and repeat process until n!=0;
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  float ans = 0;
+  int i = 0;
+  while (n != 0)
+  {
+    int bit = n & 1;
+    ans = (bit * pow(10, i)) + ans;
+    n = n >> 1; // right shift
+    i++;
+  }
+
+  cout << "answer is " << ans << endl;
+
+  return 0;
+}
