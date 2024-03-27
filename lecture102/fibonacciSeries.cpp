19294d59aa1e199e15d176ee217fa5bca305d648
Sarav <Saravjeetofficial@gmail.com>
2024-03-27 16:02:01 +0530

    fibonacci series using dp methods with space optimization

--- lecture102/fibonacciSeries.cpp
+++ lecture102/fibonacciSeries.cpp
@@ -26,3 +26,50 @@ int main()
     cout << fib(n, dp) << endl;
     return 0;
 }
+
+// bottom up or tabulation approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    // step1
+    vector<int> dp(n + 1);
+    // step2 base case observe
+    dp[1] = 1;
+    dp[0] = 0;
+    // step 3
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
+
+// space optimization
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int prev1 = 1;
+    int prev2 = 0;
+
+    if (n == 0)
+        return prev2;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    cout << prev1 << endl;
+    return 0;
+}
