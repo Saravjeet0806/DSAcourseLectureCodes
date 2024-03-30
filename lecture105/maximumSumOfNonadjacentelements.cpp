28d595e4336bcd9e17bd49216cb6768c7d71846d
Sarav <Saravjeetofficial@gmail.com>
2024-03-30 23:11:28 +0530

    space optimized

--- lecture105/maximumSumOfNonadjacentelements.cpp
+++ lecture105/maximumSumOfNonadjacentelements.cpp
@@ -61,3 +61,20 @@ int maximumNonAdjacentSum(vector<int> &nums){
     int n=nums.size();
     return solveTabulation(nums);
 }
+
+//space optimisation
+#include <bits/stdc++.h> 
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n=nums.size();
+    int prev2=0;
+    int prev1=nums[0];
+    for(int i=1; i<n; i++){
+        int incl= prev2+nums[i];
+        int excl=prev1+0;
+
+        int ans=max(incl, excl);
+        prev2=prev1;
+        prev1=ans;
+    }
+    return prev1;
+}
