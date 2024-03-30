4095371c6533bb3e120d7d68cbdbf0dbecd905f6
Sarav <Saravjeetofficial@gmail.com>
2024-03-30 06:23:36 +0530

    minimum cost climbing stiars by all methods

--- lecture103/minimumCostClimbingStair.cpp
+++ lecture103/minimumCostClimbingStair.cpp
@@ -21,3 +21,46 @@ int solve(vector<int>&cost, int n, vector<int> & dp){
        return ans;
     }
 };
+
+//using tabulation method
+class Solution {
+public:
+int solve(vector<int>&cost, int n){
+    //step1
+    vector<int> dp(n+1);
+    //step2
+    dp[0]=cost[0];
+    dp[1]=cost[1];
+    //step3
+    for(int i=2; i<n; i++){
+        dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+    }
+    return min(dp[n-1], dp[n-2]);
+}
+    int minCostClimbingStairs(vector<int>& cost) {
+       int n=cost.size();
+       return solve(cost, n);
+    }
+};
+
+//using space optimization method
+
+class Solution {
+    private: 
+    int solve(vector<int>&cost, int N){
+        int prev2= cost[0];
+        int prev1=cost[1];
+        
+        for(int i=2; i<N; i++){
+            int curr= cost[i]+ min(prev1, prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+     return min(prev1, prev2);   
+    }
+  public:
+  
+    int minCostClimbingStairs(vector<int>&cost ,int N) {
+      return solve(cost, N);
+    }
+};
