ef0d77c5a4e637731e2b2d81304ef65390f0bd82
Sarav <Saravjeetofficial@gmail.com>
2024-04-12 15:05:16 +0530

    space optimization

--- lecture115/largestsqaureArea.cpp
+++ lecture115/largestsqaureArea.cpp
@@ -65,3 +65,45 @@ public:
         return maxi;
     }
 };
+
+//space optimization solution
+class Solution{
+private: 
+  int solveSpaceOP1(vector<vector<int>> &mat, int &maxi){
+        
+        int row = mat.size();
+        int col = mat[0].size();
+        
+        vector<int> curr(col, 0);
+        vector<int> next(col+1, 0);
+        
+        for(int i = row-1; i>=0; i--){
+            for(int j = col-1; j>=0; j--){
+                
+                int right = curr[j+1];
+                int diagonal = next[j+1];
+                int down = next[j];
+                
+                if(mat[i][j] == 1){
+                    curr[j] = 1 + min(right, min(diagonal, down));
+                    
+                    maxi = max(maxi, curr[j]);
+                }
+                else
+                    curr[j] = 0;
+            }
+            
+            next = curr;
+        }
+        
+        return 0;
+    }
+public:
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+         int maxi = 0;
+        
+        solveSpaceOP1(mat, maxi);
+        
+        return maxi;
+    }
+};
