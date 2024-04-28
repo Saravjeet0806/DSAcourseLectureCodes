c7f37dd6cb7b6b40edb1cc87b832d7ed19f0959f
Sarav <Saravjeetofficial@gmail.com>
2024-04-29 05:26:43 +0530

    folders rearranged

--- lecture42/gettersetter.cpp
+++ lecture42/gettersetter.cpp
@@ -1,25 +1,26 @@
-#include <iostream>
-using namespace std;
-class Hero{
-    private: 
-    int health;
-    char level; 
-    public:
-    int getHealth(){
-          return health;
-    }
-    void setHealth(int h){
-          health = h;
-    }
+// #include <iostream>
+// using namespace std;
+// class Hero{
+//     private:
+//     int health;
+//     char level;
+//     public:
+//     int getHealth(){
+//           return health;
+//     }
+//     void setHealth(int h){
+//           health = h;
+//     }
 
-};
-int main(){
-Hero h1;  // static allocation
-Hero *h2 = new Hero; // dynamic allocation
-h1.setHealth(90);
-h2->setHealth(100);
-cout<<"The health is "<<h1.getHealth()<<endl;
-cout<<"The health is "<<(*h2).getHealth()<<endl;  //dereference operator is used for accessing dynamic allocation
-cout<<"The health is "<<h2->getHealth()<<endl;    //another way of accessing dynamic allocation
-return 0;
-}
+// };
+// int main(){
+// Hero h1;  // static allocation
+// Hero *h2 = new Hero; // dynamic allocation
+// h1.setHealth(90);
+// h2->setHealth(100);
+// cout<<"The health is "<<h1.getHealth()<<endl;
+// cout<<"The health is "<<(*h2).getHealth()<<endl;  //dereference operator is used for accessing dynamic allocation
+// cout<<"The health is "<<h2->getHealth()<<endl;    //another way of accessing dynamic allocation
+// return 0;
+// }
+
