8860520551ca9b93d4ca8eb899eb849467e0d65c
Sarav <Saravjeetofficial@gmail.com>
2024-09-23 20:02:21 +0530

    leetcode 721

---
+++ Graph/lecture97/accounts_merge.cpp
@@ -0,0 +1,41 @@
+ vector<vector<string>> accountsMerge(vector<vector<string>>& details) {
+        int n = details.size();
+        DisjointSet ds(n);
+        sort(details.begin(), details.end());
+        unordered_map<string, int> mapMailNode;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < details[i].size(); j++) {
+                string mail = details[i][j];
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;  //first time 
+                }
+                else {
+                    ds.unionBySize(i, mapMailNode[mail]);  
+                }
+            }
+        }
+
+        vector<string> mergedMail[n];
+        for (auto it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.findUPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].size() == 0) continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(details[i][0]);
+            for (auto it : mergedMail[i]) {
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+
+    }
+};
