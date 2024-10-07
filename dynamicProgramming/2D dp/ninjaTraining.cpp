87b03205a67fbda48a0bc11bbafca9c2ae61f545
Sarav <Saravjeetofficial@gmail.com>
2024-10-07 06:33:38 +0530

    space optimization of ninja training

--- dynamicProgramming/2D dp/ninjaTraining.cpp
+++ dynamicProgramming/2D dp/ninjaTraining.cpp
@@ -55,3 +55,29 @@ class Solution {
        return dp[n-1][3];
     }
 };
+
+//space optimization
+class Solution {
+  public:
+    int maximumPoints(vector<vector<int>>& arr, int n) {
+       vector<int>prev(4, 0);
+       prev[0] = max(arr[0][1], arr[0][2]);
+       prev[1]= max(arr[0][0], arr[0][2]);
+       prev[2] = max(arr[0][0], arr[0][1]);
+       prev[3] = max(arr[0][1], max(arr[0][1], arr[0][2]));
+       
+       for(int day =1; day<n; day++){
+           vector<int>temp(4,0);
+           for(int last=0; last<4; last++){
+               temp[last]= 0;
+               for(int task=0; task<3; task++){
+                   if(task!=last){
+                       temp[last] = max(temp[last], arr[day][task] + prev[task]);
+                   }
+               }
+           }
+           prev = temp;
+       }
+       return prev[3];
+    }
+};
