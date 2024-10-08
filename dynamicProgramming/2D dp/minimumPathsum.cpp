91bd47dac9767478ceea5cd5bf7c2657861c5510
Sarav <Saravjeetofficial@gmail.com>
2024-10-08 21:01:05 +0530

    space optimization solution

--- dynamicProgramming/2D dp/minimumPathsum.cpp
+++ dynamicProgramming/2D dp/minimumPathsum.cpp
@@ -48,3 +48,35 @@ int minSumPath(int n, int m, vector<vector<int>> &matrix) {
     vector<vector<int>> dp(n, vector<int>(m, -1)); // DP table to memoize results
     return minSumPathUtil(n - 1, m - 1, matrix, dp); // Start from the bottom-right corner
 }
+
+//space optimization 
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n, vector<int>(m,0));
+        vector<int>prev(m,0);
+        for(int i=0; i<n; i++){
+            vector<int>curr(m,0);
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0){
+                    curr[j]=grid[i][j];
+                }
+                else{
+                    int up= grid[i][j];
+                    if(i>0) up+=prev[j];
+                    else up+=1e9;
+
+                    int left= grid[i][j];
+                    if(j>0) left+=curr[j-1];
+                    else left+=1e9;
+
+                    curr[j]=min(up, left);
+                }
+            }
+            prev=curr;
+        }
+        return prev[m-1];
+    }
+};
