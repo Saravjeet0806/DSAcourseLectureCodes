b94201a56bf59d4832f0d4c49bb8f31f4f629857
Sarav <Saravjeetofficial@gmail.com>
2024-10-26 20:35:06 +0530

    tabulation method

--- dynamicProgramming/2D dp/minimumFallingPathsum.cpp
+++ dynamicProgramming/2D dp/minimumFallingPathsum.cpp
@@ -31,3 +31,51 @@ public:
     return mini; 
     }
 };
+
+//tabulation
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size(); //  columns
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        for (int j = 0; j < m; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+
+        // Iterate through the matrix rows starting from the second row
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int up = matrix[i][j] + dp[i - 1][j];
+                // Left diagonal
+                int leftDiagonal = matrix[i][j];
+                if (j - 1 >= 0) {
+                    leftDiagonal += dp[i - 1][j - 1];
+                } else {
+                    leftDiagonal += 1e9; // A very large negative value to
+                                         // represent an invalid path
+                }
+
+                // Right diagonal direction (if it's a valid move)
+                int rightDiagonal = matrix[i][j];
+                if (j + 1 < m) {
+                    rightDiagonal += dp[i - 1][j + 1];
+                } else {
+                    rightDiagonal += 1e9; // A very large negative value to
+                                          // represent an invalid path
+                }
+
+                // Store the maximum of the three paths in dp
+                dp[i][j] = min(up, min(leftDiagonal, rightDiagonal));
+            }
+        }
+        int mini = INT_MAX;
+        for (int j = 0; j < m; j++) {
+            mini = min(mini, dp[n - 1][j]);
+        }
+
+        return mini;
+    }
+};
