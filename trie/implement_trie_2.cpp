5132a8459a1efa021b2e970111a3d3c905c162e4
Sarav <Saravjeetofficial@gmail.com>
2025-08-12 07:34:10 +0530

    trie implementation 2

---
+++ trie/implement_trie_2.cpp
@@ -0,0 +1,110 @@
+https://www.naukri.com/code360/problems/implement-trie_1387095?leftPanelTabValue=PROBLEM
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    Node *links[26];
+    int cntEndWith = 0;
+    int cntPrefix = 0;
+
+    Node() {
+        for (int i = 0; i < 26; i++) {
+            links[i] = nullptr;
+        }
+    }
+
+    bool containsKey(char ch) {
+        return (links[ch - 'a'] != NULL);
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void put(char ch, Node *node) {
+        links[ch - 'a'] = node;
+    }
+
+    void increaseEnd() {
+        cntEndWith++;
+    }
+
+    void increasePrefix() {
+        cntPrefix++;
+    }
+
+    void deleteEnd() {
+        cntEndWith--;
+    }
+
+    void reducePrefix() {
+        cntPrefix--;
+    }
+
+    int getEnd() {
+        return cntEndWith;
+    }
+
+    int getPrefix() {
+        return cntPrefix;
+    }
+};
+
+class Trie {
+private:
+    Node *root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(const string &word) {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!node->containsKey(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(const string &word) {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (node->containsKey(word[i])) {
+                node = node->get(word[i]);
+            } else {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(const string &word) {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (node->containsKey(word[i])) {
+                node = node->get(word[i]);
+            } else {
+                return 0;
+            }
+        }
+        return node->getPrefix();
+    }
+
+    void erase(const string &word) {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (node->containsKey(word[i])) {
+                node = node->get(word[i]);
+                node->reducePrefix();
+            } else {
+                return; // Word doesn't exist
+            }
+        }
+        node->deleteEnd();
+    }
+};
