899dc79197d94c1d356637f94a9d980a5757a5a2
Sarav <Saravjeetofficial@gmail.com>
2025-09-07 07:00:03 +0530

    gfg solution

--- Graph/lecture90 TopoSort/topologicalSort.cpp
+++ Graph/lecture90 TopoSort/topologicalSort.cpp
@@ -44,3 +44,49 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)
     }
     return ans;
 }
+
+https://www.geeksforgeeks.org/problems/topological-sort/1
+class Solution {
+  public:
+  
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited, stack<int>& st) {
+        visited[node] = 1;
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited, st);
+            }
+        }
+        // Once all neighbors are done, push this node
+        st.push(node);
+    }
+   
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+       
+        // Step 1: Build adjacency list
+        vector<vector<int>> adj(V);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+        }
+        
+        // Step 2: Visited array
+        vector<int> visited(V, 0);
+        stack<int> st;
+        
+        // Step 3: Call DFS on all unvisited nodes
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, st);
+            }
+        }
+        
+        // Step 4: Pop from stack to get topo order
+        vector<int> topo;
+        while (!st.empty()) {
+            topo.push_back(st.top());
+            st.pop();
+        }
+        
+        return topo;
+        
+    }
+};
