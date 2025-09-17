b30e52a16f9a903e5b53461b701c03d1680c3b3f
Sarav <Saravjeetofficial@gmail.com>
2025-09-17 06:34:35 +0530

    gfg approach

--- Graph/lecture93/shortestPathunweightedgraph.cpp
+++ Graph/lecture93/shortestPathunweightedgraph.cpp
@@ -51,3 +51,75 @@ vector<int> shortestPath(vector<pair<int, int>> edges, int n, int m, int s, int 
     reverse(ans.begin(), ans.end());
     return ans;
 }
+
+
+https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph-having-unit-distance/1
+
+class Solution {
+  public:
+    // Function to find the shortest path from source to all other nodes
+    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+        int n = adj.size();   // number of nodes
+        vector<int> dist(n, -1); // store shortest distance, -1 means unreachable
+        queue<int> q;
+
+        dist[src] = 0;  // distance to itself is 0
+        q.push(src);
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for(int neigh : adj[node]) {
+                if(dist[neigh] == -1) { // not visited
+                    dist[neigh] = dist[node] + 1;
+                    q.push(neigh);
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+         vector<int> adj[N]; 
+        for(auto it : edges) {
+            adj[it[0]].push_back(it[1]); 
+            adj[it[1]].push_back(it[0]); 
+        }
+
+        //A dist array of size N initialised with a large number to 
+        //indicate that initially all the nodes are untraversed.    
+    
+        int dist[N];
+        for(int i = 0;i<N;i++) dist[i] = 1e9;
+        // BFS Implementation.
+        dist[src] = 0; 
+        queue<int> q;
+        q.push(src); 
+        while(!q.empty()) {
+            int node = q.front(); 
+            q.pop(); 
+            for(auto it : adj[node]) {
+                if(dist[node] + 1 < dist[it]) {
+                    dist[it] = 1 + dist[node]; 
+                    q.push(it); 
+                }
+            }
+        }
+        // Updated shortest distances are stored in the resultant array ‘ans’.
+        // Unreachable nodes are marked as -1. 
+        vector<int> ans(N, -1);
+        for(int i = 0;i<N;i++) {
+            if(dist[i] != 1e9) {
+                ans[i] = dist[i]; 
+            }
+        }
+        return ans; 
+    }
+};
