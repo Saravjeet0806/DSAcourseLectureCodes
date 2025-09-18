faaac3fd0d4ff44d66545e5eb36d71f0bb608f05
Sarav <Saravjeetofficial@gmail.com>
2025-09-18 20:59:41 +0530

    leetcode 16

---
+++ array/Revision_sheet/sorting/3sumclosest.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int result = nums[0]+nums[1]+nums[2];
+
+        for(int i=0; i<nums.size()-2; i++){
+            int l=i+1, r=nums.size()-1;
+
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(abs(target-sum)<abs(target-result)) result = sum;
+
+                if(sum==target) return target;
+
+                else if (sum<target) l++;
+                else r--;
+            }
+        }
+        return result;
+    }
+};
