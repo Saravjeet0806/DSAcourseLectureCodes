38879fbc6b50a6fd20d3c9c83609378212996dbd
Sarav <Saravjeetofficial@gmail.com>
2025-11-09 08:34:26 +0530

    duplicate and missing values

--- array/Questions/12_find_missing_repeated_value.cpp
+++ array/Questions/12_find_missing_repeated_value.cpp
@@ -27,3 +27,38 @@ public:
         return {dup, miss};
     }
 };
+
+//optimal solution
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> findTwoElement(vector<int>& arr) {
+    int n = arr.size();
+
+    // Sum of first n natural numbers
+    long long  s = (n * (n + 1)) / 2;
+    // Sum of squares of first n natural numbers
+    int ssq = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    // Subtract actual values from expected sums
+    for (int i = 0; i < n; i++) {
+        s -= arr[i];
+        ssq -= arr[i] * arr[i];
+    }
+
+    // Using the equations: missing - repeating = s
+    // missing^2 - repeating^2 = ssq
+    int missing = (s + ssq / s) / 2;
+    int repeating = missing - s;
+
+    return {repeating, missing};
+}
+
+int main() {
+    vector<int> arr = {3, 1, 3};
+    vector<int> ans = findTwoElement(arr);
+
+    cout << ans[0] << " " << ans[1] << endl;
+    return 0;
+}
